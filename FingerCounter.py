b9ce8c70b50b7d332a4333b7dea2ef82984b2585
PAyush15 <60749666+PAyush15@users.noreply.github.com>
2021-08-02 22:54:06 +0530

    Add files via upload

---
+++ FingerCounter.py
@@ -0,0 +1,59 @@
+import cv2
+import time
+import os
+import HandTrackingModule as htm
+import serial
+
+ser1 = serial.Serial('COM3', 9600)  # change according to your arduino COM port
+ser1.timeout = 1
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        totalfingers = fingers.count(1)
+        print(100 + totalfingers)
+        ser1.write(str(100 + totalfingers).encode())
+        print(totalfingers)
+
+        cv2.rectangle(img, (20, 20), (170, 400 - 235), (0, 255, 0), cv2.FILLED, )
+        cv2.putText(img, str(totalfingers), (45, 145), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 20)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+    cv2.imshow("Feed", img)
+    cv2.waitKey(1)
+# ser1.close()
